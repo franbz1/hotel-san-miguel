925cadcde55196127ac6eb834e77eccdf9009254
franbz1 <franciscorualesp15@gmail.com>
2025-01-03 13:16:23 -0500

    config inicial documentos service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { HuespedesModule } from './huespedes/huespedes.module';
+import { DocumentosModule } from './documentos/documentos.module';
 
 @Module({
-  imports: [UsuariosModule, HuespedesModule],
+  imports: [UsuariosModule, HuespedesModule, DocumentosModule],
   controllers: [],
   providers: [],
 })
