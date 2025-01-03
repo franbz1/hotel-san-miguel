10effa4903ab2e015fa9e379e1ec85be2bedf49f
franbz1 <franciscorualesp15@gmail.com>
2025-01-03 14:19:27 -0500

    Agregar PrismaModule a DocumentosModule

--- src/documentos/documentos.module.ts
+++ src/documentos/documentos.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DocumentosService } from './documentos.service';
 import { DocumentosController } from './documentos.controller';
+import { PrismaModule } from 'src/common/prisma/prisma.module';
 
 @Module({
   controllers: [DocumentosController],
   providers: [DocumentosService],
+  imports: [PrismaModule],
 })
 export class DocumentosModule {}
