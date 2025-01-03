a845de2494803d1086c5adb46c616880f49b75a2
franbz1 <franciscorualesp15@gmail.com>
2025-01-03 15:37:05 -0500

    Agregar documentación para los métodos de creación, búsqueda, actualización y eliminación en DocumentosController

--- src/documentos/documentos.controller.ts
+++ src/documentos/documentos.controller.ts
@@ -18,11 +18,24 @@ import { PaginationDto } from 'src/common/dtos/paginationDto';
 export class DocumentosController {
   constructor(private readonly documentosService: DocumentosService) {}
 
+  /**
+   * Crea un nuevo documento.
+   * `POST /documentos`
+   * @param createDocumentoDto Datos del documento a crear.
+   * @returns El documento creado.
+   */
   @Post()
   create(@Body() createDocumentoDto: CreateDocumentoDto) {
     return this.documentosService.create(createDocumentoDto);
   }
 
+  /**
+   * Busca todos los documentos por el id del huesped
+   * `GET /documentos/huesped/:huespedId`
+   * @param huespedId ID del huesped.
+   * @param paginationDto Datos de paginación.
+   * @returns Documentos[] con los documentos y metadatos de paginación.
+   */
   @Get('huesped/:huespedId')
   findAll(
     @Param('huespedId', ParseIntPipe) huespedId: number,
@@ -31,11 +44,24 @@ export class DocumentosController {
     return this.documentosService.findAll(huespedId, paginationDto);
   }
 
+  /**
+   * Busca un documento por su ID.
+   * `GET /documentos/:id`
+   * @param id ID del documento.
+   * @returns El documento encontrado.
+   */
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.documentosService.findOne(id);
   }
 
+  /**
+   * Actualiza los datos de un documento por su ID.
+   * `PATCH /documentos/:id`
+   * @param id ID del documento.
+   * @param updateDocumentoDto Datos para actualizar.
+   * @returns El documento actualizado.
+   */
   @Patch(':id')
   update(
     @Param('id', ParseIntPipe) id: number,
@@ -44,6 +70,12 @@ export class DocumentosController {
     return this.documentosService.update(id, updateDocumentoDto);
   }
 
+  /**
+   * Elimina un documento por su ID.
+   * `DELETE /documentos/:id`
+   * @param id ID del documento.
+   * @returns El documento eliminado.
+   */
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.documentosService.remove(id);
