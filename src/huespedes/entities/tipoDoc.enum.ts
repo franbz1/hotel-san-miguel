e654b3d94e671973a9b89b2a79b613e3188fbd7f
franbz1 <franciscorualesp15@gmail.com>
2025-01-04 21:22:42 -0500

    Actualizar los valores del enum TipoDoc para reflejar nuevos códigos de documentos

--- src/huespedes/entities/tipoDoc.enum.ts
+++ src/huespedes/entities/tipoDoc.enum.ts
@@ -2,10 +2,13 @@
  * Es un tipo de documento que puede tener un huesped
  */
 export enum TipoDoc {
-  CC = 'C.C.',
-  CE = 'C.E.',
-  TI = 'T.I.',
-  PASAPORTE = 'Pasaporte',
-  PEP = 'PEP',
-  DNI = 'DNI',
+  CC = '1',
+  TI = '2',
+  PASAPORTE = '3',
+  PEP = '4',
+  CE = '5',
+  DNI = '6',
+  DOCUMENTO_EXTRANJERO = '10',
+  CARNET_DIPLOMATICO = '46',
+  PPT = '52',
 }
