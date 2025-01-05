83a53e17280531a56029043ddb0b3213ceb424e5
franbz1 <franciscorualesp15@gmail.com>
2025-01-05 10:37:15 -0500

    base para crud facturas

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { HuespedesModule } from './huespedes/huespedes.module';
 import { DocumentosModule } from './documentos/documentos.module';
+import { FacturasModule } from './facturas/facturas.module';
 
 @Module({
-  imports: [UsuariosModule, HuespedesModule, DocumentosModule],
+  imports: [UsuariosModule, HuespedesModule, DocumentosModule, FacturasModule],
   controllers: [],
   providers: [],
 })
