f63f25f48cc8a42b1b1d6a31eed626be57b75c27
franbz1 <franciscorualesp15@gmail.com>
2025-01-07 21:08:30 -0500

    Actualizar mensaje de error en notFoundError para mayor claridad sobre el elemento no encontrado

--- src/common/errors/notfoundError.ts
+++ src/common/errors/notfoundError.ts
@@ -1,10 +1,10 @@
 import { NotFoundException } from '@nestjs/common';
 
 /**
- * Error de Usuario no encontrado customizado.
- * @param id ID del usuario.
+ * Error de no encontrado customizado.
+ * @param id ID del elemento que no se encuentra.
  * @returns Objeto con el mensaje de error.
  */
 export default function notFoundError(id: number) {
-  throw new NotFoundException(`No se encontró el usuario con ID: ${id}`);
+  throw new NotFoundException(`No se encontró el elemento con el ID: ${id}`);
 }
