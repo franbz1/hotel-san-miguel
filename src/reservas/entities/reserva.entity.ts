3e37705643b51c1ded69c55370a81cb103ea431a
piandas <santiagobustosp@gmail.com>
2025-01-09 12:26:44 -0500

    Agregar propiedades a la entidad Reserva para gestionar información de reservas

--- src/reservas/entities/reserva.entity.ts
+++ src/reservas/entities/reserva.entity.ts
@@ -1,3 +1,18 @@
 export class Reserva {
-    
+  id: number;
+  fecha_inicio: Date;
+  fecha_fin: Date;
+  estado: string;
+  pais_procedencia: string;
+  departamento_procedencia: string;
+  ciudad_procedencia: string;
+  pais_destino: string;
+  motivo_viaje: string;
+  check_in: Date;
+  check_out: Date;
+  costo: number;
+  numero_acompaniantes: number;
+  habitacion_id: number;
+  huesped_id: number;
+  factura_id: number;
 }
