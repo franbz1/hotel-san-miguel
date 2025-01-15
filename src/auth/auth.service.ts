1b91cab72af639298de66ce44f538d1ec74c5cf6
franbz1 <franciscorualesp15@gmail.com>
2025-01-14 21:00:26 -0500

    agregar lógica de autenticación en AuthService y modificar AuthController para recibir LoginDto y usar bcrypt compare

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsuariosService } from 'src/usuarios/usuarios.service';
+import { LoginDto } from './dto/loginDto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly usuariosService: UsuariosService) {}
-  async login() {
-    return 'login';
+
+  async login(loginDto: LoginDto) {
+    try {
+      const usuario = await this.usuariosService.findByNombre(loginDto.nombre);
+      const passwordMatch = await bcrypt.compare(
+        loginDto.password,
+        usuario.password,
+      );
+
+      if (!passwordMatch) throw new UnauthorizedException();
+
+      return { usuarioId: usuario.id, token: 'token' };
+    } catch (error) {
+      throw new UnauthorizedException(error.code);
+    }
   }
 
   async logout() {
