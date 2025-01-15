115185c504ef1629f1f1df9c536420152b8d6fc8
franbz1 <franciscorualesp15@gmail.com>
2025-01-14 21:32:56 -0500

    agregar lógica de autenticación en AuthService para generar un token JWT y devolver información del usuario

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,11 +2,21 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsuariosService } from 'src/usuarios/usuarios.service';
 import { LoginDto } from './dto/loginDto';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usuariosService: UsuariosService) {}
+  constructor(
+    private readonly usuariosService: UsuariosService,
+    private readonly jwtService: JwtService,
+  ) {}
 
+  /**
+   * Autentica un usuario.
+   * @param loginDto Datos del usuario a autenticar.
+   * @returns Token de autenticación.
+   * @throws UnauthorizedException si el usuario no existe o la contraseña es incorrecta.
+   */
   async login(loginDto: LoginDto) {
     try {
       const usuario = await this.usuariosService.findByNombre(loginDto.nombre);
@@ -17,7 +27,14 @@ export class AuthService {
 
       if (!passwordMatch) throw new UnauthorizedException();
 
-      return { usuarioId: usuario.id, token: 'token' };
+      const payload = { id: usuario.id, rol: usuario.rol };
+
+      return {
+        token: await this.jwtService.signAsync(payload),
+        usuarioId: usuario.id,
+        nombre: usuario.nombre,
+        rol: usuario.rol,
+      };
     } catch (error) {
       throw new UnauthorizedException(error.code);
     }
