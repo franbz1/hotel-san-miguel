f3c730ae4ee1f02457346f4852572d3f3faabf1d
franbz1 <franciscorualesp15@gmail.com>
2025-01-14 21:08:07 -0500

    agregar soporte para JWT en AuthModule y actualizar configuración de variables de entorno

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsuariosModule } from 'src/usuarios/usuarios.module';
+import { JwtModule } from '@nestjs/jwt';
+import { envs } from 'src/config/envs';
 
 @Module({
   providers: [AuthService],
   controllers: [AuthController],
-  imports: [UsuariosModule],
+  imports: [
+    UsuariosModule,
+    JwtModule.register({
+      global: true,
+      secret: envs.jwtSecret,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
 })
 export class AuthModule {}
