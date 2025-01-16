e597d56271bb99a050b61f4f5c89270d3f391100
franbz1 <franciscorualesp15@gmail.com>
2025-01-16 17:58:14 -0500

    agregar verificación de estado del formulario en el guardia LinkFormulario y manejo de tokens expirados y completados

--- src/auth/guards/linkFormulario.guard.ts
+++ src/auth/guards/linkFormulario.guard.ts
@@ -8,10 +8,14 @@ import { JwtService, TokenExpiredError } from '@nestjs/jwt';
 import { Request } from 'express';
 
 import { envs } from 'src/config/envs';
+import { RegistroFormularioService } from 'src/registro-formulario/registro-formulario.service';
 
 @Injectable()
 export class LinkFormularioGuard implements CanActivate {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly registroFormularioService: RegistroFormularioService,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
@@ -24,9 +28,28 @@ export class LinkFormularioGuard implements CanActivate {
         secret: envs.jwtSecret,
       });
 
+      const formulario = await this.registroFormularioService.findOne(
+        payload.id,
+      );
+
+      if (!formulario || formulario.expirado || formulario.completado) {
+        throw new UnauthorizedException();
+      }
+
       request['usuario'] = payload;
-    } catch (e) {
-      if (e instanceof TokenExpiredError) {
+    } catch (error) {
+      if (error instanceof TokenExpiredError) {
+        const payload = await this.jwtService.verifyAsync(token, {
+          secret: envs.jwtSecret,
+          ignoreExpiration: true,
+        });
+
+        if (payload?.id) {
+          await this.registroFormularioService.update(payload.id, {
+            expirado: true,
+          });
+        }
+
         throw new UnauthorizedException('Token expirado');
       }
       throw new UnauthorizedException();
