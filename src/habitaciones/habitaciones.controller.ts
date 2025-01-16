01eb1b487e405791c0f9be82e6d11e291a3a722d
franbz1 <franciscorualesp15@gmail.com>
2025-01-15 22:20:02 -0500

    reemplazar AuthGuard por el decorador Auth con roles ADMINISTRADOR y CAJERO en el controlador de habitaciones

--- src/habitaciones/habitaciones.controller.ts
+++ src/habitaciones/habitaciones.controller.ts
@@ -15,22 +15,30 @@ import { CreateHabitacionDto } from './dto/create-habitacion.dto';
 import { UpdateHabitacionDto } from './dto/update-habitacion.dto';
 import { PaginationDto } from 'src/common/dtos/paginationDto';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { Role } from 'src/usuarios/entities/rol.enum';
 
 @UseGuards(AuthGuard)
 @Controller('habitaciones')
 export class HabitacionesController {
   constructor(private readonly habitacionesService: HabitacionesService) {}
 
+  @Roles(Role.ADMINISTRADOR)
+  @UseGuards(AuthGuard)
   @Post()
   create(@Body() createHabitacionDto: CreateHabitacionDto) {
     return this.habitacionesService.create(createHabitacionDto);
   }
 
+  @Roles(Role.ADMINISTRADOR, Role.CAJERO)
+  @UseGuards(AuthGuard)
   @Get()
   findAll(@Query() paginationDto: PaginationDto) {
     return this.habitacionesService.findAll(paginationDto);
   }
 
+  @Roles(Role.ADMINISTRADOR, Role.CAJERO)
+  @UseGuards(AuthGuard)
   @Get('numero_habitacion/:numeroHabitacion')
   findByNumeroHabitacion(
     @Param('numeroHabitacion', ParseIntPipe) numeroHabitacion: number,
@@ -38,11 +46,15 @@ export class HabitacionesController {
     return this.habitacionesService.findByNumeroHabitacion(numeroHabitacion);
   }
 
+  @Roles(Role.ADMINISTRADOR, Role.CAJERO)
+  @UseGuards(AuthGuard)
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.habitacionesService.findOne(id);
   }
 
+  @Roles(Role.ADMINISTRADOR, Role.CAJERO)
+  @UseGuards(AuthGuard)
   @Patch(':id')
   update(
     @Param('id', ParseIntPipe) id: number,
@@ -51,6 +63,8 @@ export class HabitacionesController {
     return this.habitacionesService.update(id, updateHabitacionDto);
   }
 
+  @Roles(Role.ADMINISTRADOR)
+  @UseGuards(AuthGuard)
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.habitacionesService.remove(id);
