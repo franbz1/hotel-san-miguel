cef33563a52a379e0e92607fe6eceb13370a7b98
franbz1 <franciscorualesp15@gmail.com>
2025-01-19 15:42:51 -0500

    Cambios en los campos necesarios para TRA

--- src/TRA/dto/postHuespedPrincipal.ts
+++ src/TRA/dto/postHuespedPrincipal.ts
@@ -2,7 +2,5 @@ import { CreateHuespedPrincipalTraDto } from './huespedPrincipalDto';
 
 export class PostHuespedPrincipalDto extends CreateHuespedPrincipalTraDto {
   nombre_establecimiento: string;
-  nit_establecimiento: string;
   rnt_establecimiento: string;
-  n_habitaciones: string;
 }
