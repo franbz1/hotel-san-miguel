f67dabd419120e3d9284919ad4752f6090e4bab6
franbz1 <franciscorualesp15@gmail.com>
2025-01-18 23:37:03 -0500

    Agregar campo n_habitaciones al DTO de PostHuespedPrincipal

--- src/TRA/dto/postHuespedPrincipal.ts
+++ src/TRA/dto/postHuespedPrincipal.ts
@@ -4,4 +4,5 @@ export class PostHuespedPrincipalDto extends CreateHuespedPrincipalTraDto {
   nombre_establecimiento: string;
   nit_establecimiento: string;
   rnt_establecimiento: string;
+  n_habitaciones: string;
 }
