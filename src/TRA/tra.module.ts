2aa77ff41993ff3092f3cabb52ecf2793f87922d
franbz1 <franciscorualesp15@gmail.com>
2025-01-18 23:37:13 -0500

    Agregar módulo Habitaciones al módulo TRA

--- src/TRA/tra.module.ts
+++ src/TRA/tra.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TraService } from './tra.service';
 import { HttpModule } from '@nestjs/axios';
+import { HabitacionesModule } from 'src/habitaciones/habitaciones.module';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, HabitacionesModule],
   providers: [TraService],
   exports: [TraService],
 })
