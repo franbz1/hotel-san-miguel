a3ea3ce668017db189112f287b2205c399a387f2
franbz1 <franciscorualesp15@gmail.com>
2025-01-27 21:18:01 -0500

    Agregar SireController y registrar en SireModule

--- src/sire/sire.module.ts
+++ src/sire/sire.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { SireService } from './sire.service';
+import { SireController } from './sire.controller';
 
 @Module({
   providers: [SireService],
+  controllers: [SireController],
 })
 export class SireModule {}
