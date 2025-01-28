886e410febba615d5557f63de901d3fe9981329b
franbz1 <franciscorualesp15@gmail.com>
2025-01-27 21:18:24 -0500

    Agregar logging y soporte para HuespedSireDto en SireService

--- src/sire/sire.service.ts
+++ src/sire/sire.service.ts
@@ -1,14 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { huespedesSireDto } from './dtos/HuespedSireDto';
+import { TxtIndentFile } from 'src/common/create-doc/factory/TxtIndentFile';
 
 @Injectable()
 export class SireService {
   constructor() {}
 
-  async uploadToSire(file: any) {
+  private readonly logger = new Logger(SireService.name);
+
+  async uploadToSire(huespedesSireDto: huespedesSireDto[]) {
     
   }
 
-  async uploadOneToSire(huespedTraDto: any) {
+  async uploadOneToSire(huespedeSireDto: huespedesSireDto) {
+    this.logger.debug(huespedeSireDto);
 
+    const test = new TxtIndentFile();
+    const data = await test.generate([huespedeSireDto]);
+    this.logger.debug(data);
   }
 }
