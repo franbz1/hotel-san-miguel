727afca68b5d7c1e6eebfebfa3e1eec8b154bf82
franbz1 <franciscorualesp15@gmail.com>
2025-01-29 16:12:48 -0500

    Corregir el nombre de la clase HuespedesSireDto en SireController para seguir la convención de nomenclatura

--- src/sire/sire.controller.ts
+++ src/sire/sire.controller.ts
@@ -1,13 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { SireService } from './sire.service';
-import { huespedesSireDto } from './dtos/HuespedSireDto';
+import { HuespedesSireDto } from './dtos/HuespedSireDto';
 
 @Controller('sire')
 export class SireController {
   constructor(private readonly sireService: SireService) {}
 
   @Post('test')
-  test(@Body() data: huespedesSireDto) {
+  test(@Body() data: HuespedesSireDto) {
     return this.sireService.uploadOneToSire(data);
   }
 }
