96cb43322db2f2b004be3739a6b08e9e7adaae23
franbz1 <franciscorualesp15@gmail.com>
2025-01-29 16:46:17 -0500

    implementacion de creacion de un archivo txt en sire service funcional

--- src/sire/sire.service.ts
+++ src/sire/sire.service.ts
@@ -1,22 +1,71 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { huespedesSireDto } from './dtos/HuespedSireDto';
-import { TxtIndentFile } from 'src/common/create-doc/factory/TxtIndentFile';
+import { HuespedesSireDto } from './dtos/HuespedSireDto';
+import { SIRE_CREDENCIALES } from 'src/common/constants/SireCredenciales';
+import puppeteer from 'puppeteer-core';
+import { CreateDocService } from 'src/common/create-doc/create-doc.service';
 
 @Injectable()
 export class SireService {
-  constructor() {}
+  constructor(private readonly createDocService: CreateDocService) {}
 
   private readonly logger = new Logger(SireService.name);
 
-  async uploadToSire(huespedesSireDto: huespedesSireDto[]) {
-    
+  /**
+   * Sube un arreglo de huespedesSire al sire
+   * @param huespedesSireDto
+   * @returns Promise<boolean> true si los huespedes se subieron correctamente
+   */
+  async uploadAllToSire(
+    huespedesSireDto: HuespedesSireDto[],
+  ): Promise<boolean> {
+    return Promise.resolve(false);
+    //TODO: Hacer el upload de multiples huespedes a sire
   }
 
-  async uploadOneToSire(huespedeSireDto: huespedesSireDto) {
-    this.logger.debug(huespedeSireDto);
+  /**
+   * Toma un huesped lo convertirá a un archivo de texto y lo subirá a sire
+   * @param huespedSireDto
+   * @returns true si el archivo se subió correctamente
+   */
+  async uploadOneToSire(huespedSireDto: HuespedesSireDto): Promise<boolean> {
+    const data = [
+      {
+        codigoDelHotel: SIRE_CREDENCIALES.codigoHotelSire,
+        codigoDeCiudad: SIRE_CREDENCIALES.codigoCiudadSire,
+        ...huespedSireDto,
+      },
+    ];
+    try {
+      const rutaFile = await this.createDocService
+        .getGenerator('txtIndent')
+        .generate(data);
+      this.logger.debug(`Ruta del archivo generado: ${rutaFile}`);
 
-    const test = new TxtIndentFile();
-    const data = await test.generate([huespedeSireDto]);
-    this.logger.debug(data);
+      return await this.uploadFileToSire(rutaFile);
+    } catch (error) {
+      this.logger.error(`Error al subir el archivo a sire: ${error}`);
+    }
+  }
+
+  /**
+   * Toma un archivo de huespedes y lo subirá a sire mediante puppeteer
+   * @param fileRute
+   * @returns
+   */
+  async uploadFileToSire(fileRute: string): Promise<boolean> {
+    return Promise.resolve(false);
+    //TODO: Hacer el upload de archivo a sire
+    /*     if (!fileRute) {
+      throw new Error('No se pudo subir el archivo a sire');
+    }
+
+    try {
+      const browser = await puppeteer.launch({
+        headless: true,
+      });
+
+    } catch (error) {
+      
+    } */
   }
 }
