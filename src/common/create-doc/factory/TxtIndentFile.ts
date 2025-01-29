9ec876dd162e92094db096ef9ae640033074db7b
franbz1 <franciscorualesp15@gmail.com>
2025-01-29 16:12:29 -0500

    Agregar manejo de errores y validación en la generación de archivos en TxtIndentFile

--- src/common/create-doc/factory/TxtIndentFile.ts
+++ src/common/create-doc/factory/TxtIndentFile.ts
@@ -1,21 +1,22 @@
-import { huespedesSireDto } from 'src/sire/dtos/HuespedSireDto';
 import { CreateDocInterface } from '../create-doc-interface';
 import { writeFile } from 'fs';
 
 export class TxtIndentFile implements CreateDocInterface {
-  async generate(data: huespedesSireDto[]): Promise<string> {
-    const rows = data.map((row) => Object.values(row).join('\t')).join('\n');
-
-    await writeFile(
-      'C:\\Users\\programacion\\Documents\\Hotel san miguel\\Codigo\\hotel-san-miguel\\src\\common\\create-doc\\factory\\test.txt',
-      rows,
-      'utf8',
-      (err) => {
+  generate(data: any[]): Promise<string> {
+    if (data.length === 0) {
+      throw new Error('No data to generate');
+    }
+    try {
+      const rows = data.map((row) => Object.values(row).join('\t')).join('\n');
+      const rute =
+        'C:\\Users\\programacion\\Documents\\Hotel san miguel\\Codigo\\hotel-san-miguel\\src\\common\\create-doc\\factory\\test.txt';
+      writeFile(rute, rows, 'utf8', (err) => {
         if (err) throw err;
-      },
-    );
-
-    return Promise.resolve(rows);
+      });
+      return Promise.resolve(rute);
+    } catch (error) {
+      throw new Error(`Error al generar el archivo ${error}`);
+    }
   }
   getExtensionFile(): string {
     throw new Error('Method not implemented.');
