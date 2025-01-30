aed8197a756245cf92d18f1a39f863e19043a952
Piandas <santiagobustosp@gmail.com>
2025-01-30 13:30:57 -0500

    Agregar configuración de Swagger para la documentación de la API al maint.ts

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { envs } from './config/envs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('App');
 
+  // Aplicar pipes globales para validación
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
+
+  // Configurar Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Hotel San Miguel API') // Título de la API
+    .setDescription('Documentación de la API del Hotel San Miguel') // Descripción
+    .setVersion('1.0.0') // Versión
+    .addBearerAuth() // Autenticación Bearer (JWT)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Ruta de la documentación en "/api"
 
   await app.listen(envs.port);
   logger.log(`Application is running on: ${envs.port}`);
