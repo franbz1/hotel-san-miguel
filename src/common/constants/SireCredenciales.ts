94e1f07065a5bb7d177f9ff2a2074b1b6b3e1464
franbz1 <franciscorualesp15@gmail.com>
2025-01-30 21:37:28 -0500

    Agregar propiedades de usuario y credenciales al interfaz SireCredenciales

--- src/common/constants/SireCredenciales.ts
+++ src/common/constants/SireCredenciales.ts
@@ -1,6 +1,11 @@
+import { TipoDocumentoSireUpload } from '../enums/tipoDocSireUpload';
+
 interface SireCredenciales {
   codigoHotelSire: number;
   codigoCiudadSire: number;
+  usuarioSire: string;
+  passwordSire: string;
+  tipoDocSireUpload: TipoDocumentoSireUpload;
 
   SireUrl: string;
 }
@@ -8,9 +13,15 @@ interface SireCredenciales {
 const codigoHotelSire = 1;
 const codigoCiudadSire = 1;
 const SireUrl = 'https://apps.migracioncolombia.gov.co/sire/public/login.jsf';
+const usuarioSire = '12991769';
+const passwordSire = 'arcangel';
+const tipoDocSireUpload = TipoDocumentoSireUpload.CC;
 
 export const SIRE_CREDENCIALES: SireCredenciales = {
   codigoHotelSire,
   codigoCiudadSire,
   SireUrl,
+  usuarioSire,
+  passwordSire,
+  tipoDocSireUpload,
 };
