37d831782c266847f67f48c25b06683a7dcaab52
franbz1 <franciscorualesp15@gmail.com>
2025-01-30 21:38:01 -0500

    reactivar validacion

--- src/registro-formulario/registro-formulario.controller.ts
+++ src/registro-formulario/registro-formulario.controller.ts
@@ -29,7 +29,7 @@ export class RegistroFormularioController {
     );
   }
 
-  //@Auth(Role.ADMINISTRADOR, Role.CAJERO)
+  @Auth(Role.ADMINISTRADOR, Role.CAJERO)
   @Get()
   createLinkTemporal() {
     return this.linkFormularioService.createLinkTemporal();
