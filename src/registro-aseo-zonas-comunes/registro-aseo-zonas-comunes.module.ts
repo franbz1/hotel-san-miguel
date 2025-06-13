8b616ef6ba66f2b99a0654b719ca021cf94ec2ec
francisco ruales <franciscorualesp15@gmail.com>
2025-06-13 15:22:47 -0500

    agregado PrismaModule y AuthModule al módulo de registro de aseo de zonas comunes

--- src/registro-aseo-zonas-comunes/registro-aseo-zonas-comunes.module.ts
+++ src/registro-aseo-zonas-comunes/registro-aseo-zonas-comunes.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RegistroAseoZonasComunesService } from './registro-aseo-zonas-comunes.service';
 import { RegistroAseoZonasComunesController } from './registro-aseo-zonas-comunes.controller';
+import { PrismaModule } from 'src/common/prisma/prisma.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [RegistroAseoZonasComunesController],
+  imports: [PrismaModule, AuthModule],
   providers: [RegistroAseoZonasComunesService],
 })
 export class RegistroAseoZonasComunesModule {}
